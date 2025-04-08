3560515c89b5840e09d6ca6c7d8000230c0272e3
duque das edits <92229705+JosueHTML2@users.noreply.github.com>
2025-04-08 12:19:57 +0100

    Adding Readme file

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-const a = require('./index.js');
-const client = new a.RestAPI({ token: '/mYcFkTs@hQll-a', baseURL: "http://localhost:3000/api/v1" });
+const mod = require('./index.js');
+const client = new mod.RestAPI({ token: '/mYcFkTs@hQll-a', baseURL: "http://localhost:3000/api/v1" });
 
 (async () => {
     const user = await client.users.create({
         player: { id: "877598927149490186" }
     });
-
-    // client.users.deleteAll()
-    //await client.users.deleteUser("877598927149490186");
-
     const userWins = await user.addWins();
-
     console.log(userWins);
-    
 })();
