53e7dcac807d44e722e3ca0273198952af5c1bb6
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-04-30 18:00:04 +0100

    chore: test file

--- test.test.js
+++ test.test.js
@@ -2,16 +2,18 @@ const { REST, RestAPI, BaseClass, UsersRoutes, User } = require("./index.js");
 
 const client = new RestAPI({
   token: "/mYcFkTs@hQll-a",
-  baseURL: "https://duque-bot-api.up.railway.app/api/v1",
+  baseURL: "http://localhost:3000/api/v1/",
 });
 
 client.users.init().then(async (_) => {
   console.log("User's routes initialized successfully");
-
-  const user = client.users.cache.get("877598927149490186");
+  const users = client.users;
+  const user = await client.users.create({ player: { id: "877598927149490186" } });
   //await user.increment("wins", 10)
-  //await user.set("blacklisted", true);
+  await user.set("blacklisted", true);
+
+  user.increment("wins", 10).then(w => console.log(w));
+  await user.set("blacklisted", false);
+
   console.log(user);
-  
 });
- 
