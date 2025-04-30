e20e07445b7ee3d5f58189f358d58561748360eb
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-04-30 18:50:51 +0100

    feat: using undici faster  requests

--- src/rest/requester.js
+++ src/rest/requester.js
@@ -1,46 +1,28 @@
 // src/rest/requester.js
-const axios = require('axios');
+const { request } = require('undici');
 
 module.exports = async function requester(method, url, token, data) {
-  const config = {
-    method,
-    url,
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    },
-    data,
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
   };
+
+  const options = {
+    method,
+    headers,
+    body: data ? JSON.stringify(data) : undefined, // If data exists, stringifying it
+  };
+
   try {
-    const res = await axios(config);
-    return res.data.data;
+    const res = await request(url, options);
+    const responseData = await res.body.json(); // Parse the response body as JSON
+    return responseData.data;
   } catch (error) {
-    if (error.response) {
-      // The server responded with a status code that falls out of the range of 2xx
-      const status = error.response.status;
-      switch (status) {
-        case 400:
-          console.error('Bad Request: Invalid syntax or data');
-          break;
-        case 401:
-          console.error('Unauthorized: Authentication required');
-          break;
-        case 404:
-          console.error('Not Found: The requested resource could not be found');
-          break;
-        case 500:
-          console.error('Server Error: There was an issue with the server');
-          break;
-        default:
-          console.error(`Unexpected Error: ${status}`);
-      }
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error('No response received from the URL');
-    } else {
-      // Something else caused the error
+    if (error instanceof Error) {
       console.error('Error:', error.message);
+    } else {
+      console.error('Unexpected error occurred', error);
     }
     return error;
-  };
-}
+  }
+};
