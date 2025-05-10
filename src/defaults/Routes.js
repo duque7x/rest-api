3b1df9133dcf665e6abb4b6e78fbe10b37de82c9
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-05-11 00:25:22 +0100

    Update Routes.js

--- src/defaults/Routes.js
+++ src/defaults/Routes.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //base: "https://duque-bot-api.up.railway.app/api/v1",
-    base: "http://localhost:3000/api/v1",
+    base: "https://duque-bot-api.up.railway.app/api/v1",
+    //base: "http://localhost:3000/api/v1",
     users: "/users",
     matches: "/matches",
     guilds: "/guilds",
